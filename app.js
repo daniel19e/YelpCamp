46d7b4932151e93d258c1b09a3e76eaf6da80cc5
daniel19e <esc.daniel91@gmail.com>
2021-06-05 23:36:56 -0400

    basic app

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const app = express()
+const mongoose = require('mongoose')
 const path = require('path')
 const Campground = require('./models/campground')
 
